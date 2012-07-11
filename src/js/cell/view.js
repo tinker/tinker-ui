af7214159c65bbaacd4c4d869203df913b4fb2bb
Chiel Kunkels <chiel@kunkels.me>
2012-07-11 23:29:06 +0100

    Worked on layouts some more
    Layouts can now be rendered based on a json spec

---
+++ src/js/cell/view.js
@@ -0,0 +1,53 @@
+// Chiel Kunkels (@chielkunkels)
+'use strict';
+
+// exposed
+var Cell = new Class({
+	/**
+	 * Initialise a new cell object
+	 * @param element wrapper The wrapper element
+	 */
+	initialize: function(wrapper){
+		this.index = index++;
+		this.wrapper = wrapper;
+		this.outer = new Element('section.cell#cell'+this.index);
+		this.inner = new Element('div.cell-inner').inject(this.outer);
+		this.outer.inject(this.wrapper);
+	},
+
+	/**
+	 * @return element The outer element of the cell
+	 */
+	getOuter: function(){
+		return this.outer;
+	},
+
+	/**
+	 * @return element The inner element of the cell
+	 */
+	getInner: function(){
+		return this.inner;
+	},
+
+	/**
+	 * @return object A set of coordinate
+	 */
+	getCoords: function(){
+		var pos = this.inner.getPosition(this.wrapper),
+			size = this.inner.getSize();
+
+		return {
+			x1: pos.x,
+			y1: pos.y,
+			x2: pos.x + size.x,
+			y2: pos.y + size.y
+		};
+	}
+});
+
+// private
+var index = 0;
+
+// export
+module.exports = Cell;
+
--- src/js/layout/client.js
+++ src/js/layout/client.js
@@ -2,40 +2,146 @@
 'use strict';
 
 // required modules
-var events = require('./../events/model');
+var events = require('./../events/model'),
+	config = require('./../config/model'),
+	Cell = require('./../cell/view');
 
 // exposed
 var layout = {};
 
-// TODO: remove this
-slab.register({
-	layout: function() {
-		return ['<form method="post" action="#sandbox" target="sandbox" class="wrapper">',
-				'<header>',
-					'<section class="rgn rgn-tl"></section>',
-					'<section class="rgn rgn-tc"></section>',
-					'<section class="rgn rgn-tr"></section>',
-				'</header>',
-				'<section class="body"></section>',
-				'<footer>',
-					'<section class="rgn rgn-bl"></section>',
-					'<section class="rgn rgn-bc"></section>',
-					'<section class="rgn rgn-br"></section>',
-				'</footer>',
-			'</form>'].join('');
-	}
-});
-
 // private
-var regions = {};
+var regions = [], curLayout = null, layoutPicker, body, cells,
+	widths = [], heights = [];
 
+// builds up the base layout
 var build = function(){
-	var html = slab.load('layout')();
-
-	document.body.set('html', html);
-
+	document.body.set('html', slab.load('layout')());
+	body = $('body');
+	cells = [
+		new Cell(body),
+		new Cell(body),
+		new Cell(body),
+		new Cell(body)
+	];
 	regions = $$('.rgn');
-	console.log(regions);
+
+	//
+	if (!config.layouts.length) {
+		console.log('No layouts found!');
+	}
+
+	layout.activate(0);
+
+	buildLayoutPicker();
+};
+
+/**
+ *
+ */
+var buildLayoutPicker = function(){
+	layout.addToRegion(slab.load('layoutPicker')());
+};
+
+/**
+ * Recalculate col/row sizes
+ */
+var recalc = function(){
+	var l = config.layouts[curLayout], i = 0, percent,
+		bSize = body.getSize(), opWidth = bSize.x/100, opHeight = bSize.y/100,
+		width, height, consumed = 0;
+
+	widths = [];
+	heights = [];
+
+	for (; i < l.cols.length; i++){
+		if (l.cols[i+1]) {
+			percent = l.cols[i+1] - l.cols[i];
+			width = Math.round(percent * opWidth);
+			widths.push(width);
+			consumed += width;
+		} else {
+			widths.push(bSize.x - consumed);
+		}
+	}
+
+	i = 0;
+	consumed = 0
+	for (; i < l.rows.length; i++){
+		if (l.rows[i+1]) {
+			percent = l.rows[i+1] - l.rows[i];
+			height = Math.round(percent * opHeight);
+			heights.push(height);
+			consumed += height;
+		} else {
+			heights.push(bSize.y - consumed);
+		}
+	}
+};
+
+/**
+ * Reflow the layout
+ */
+var reflow = function(){
+	var i = 0, c, coords;
+	for (; i < config.layouts[curLayout].cells.length; i++){
+		c = config.layouts[curLayout].cells[i];
+		coords = cellCoords(c);
+		cells[i].getOuter().setStyles({
+			top: coords.y1,
+			left: coords.x1,
+			width: coords.x2 - coords.x1,
+			height: coords.y2 - coords.y1
+		});
+	}
+};
+
+/**
+ * Calculate coords of a cell
+ * @param object spec Specification of how the cell should look
+ * @return object Coordinates
+ */
+var cellCoords = function(spec){
+	var i = 0, j = 0, x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+
+	for (; i < spec[0]; i++){
+		x1 += widths[i];
+	}
+	for (; j < spec[2]; i++, j++){
+		x2 += widths[i];
+	}
+	x2 += x1;
+
+	i = 0;
+	j = 0;
+	for (; i < spec[1]; i++){
+		y1 += heights[i];
+	}
+	for (; j < spec[3]; i++, j++){
+		y2 += heights[i];
+	}
+	y2 += y1;
+
+	return {x1: x1, y1: y1, x2: x2, y2: y2}
+};
+
+/**
+ * Add a chunk of html or an element node to a region
+ */
+layout.addToRegion = function(){
+
+};
+
+/**
+ * Activate a layout by index
+ */
+layout.activate = function(index){
+	if (!config.layouts[index]) {
+		return;
+	}
+
+	curLayout = index;
+	recalc();
+	reflow();
 };
 
 // events
