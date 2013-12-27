4e558abeaa7c416e85680d30965692f05c327ea6
Chiel Kunkels <chiel@kunkels.me>
2013-12-27 21:12:45 +0100

    Enable loading of bundles

---
+++ src/js/lib/bundles.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var isObject = require('mout/lang/isObject'),
+	editors = require('./../ui/editor'),
+	execute = require('./actions/execute');
+
+var types = ['markup', 'style', 'behavior'];
+
+/**
+ * Load a bundle
+ */
+var load = function(bundle){
+	if (!isObject(bundle)) return false;
+	if (isObject(bundle.code)){
+		var i, len = types.length, type;
+		for (i = 0; i < len; i++){
+			type = types[i];
+			if (!bundle.code[type] || !isObject(bundle.code[type])) continue;
+
+			editors[type].setMode(bundle.code[type].mode);
+			editors[type].setValue(bundle.code[type].body);
+		}
+	}
+
+	execute();
+};
+
+module.exports = {
+	load: load
+};
+
--- src/js/tinker.js
+++ src/js/tinker.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var init = require('./lib/init'),
+	bundles = require('./lib/bundles'),
 	layouts = require('./lib/layouts');
 
 module.exports = {
 	init: init,
+	bundles: {
+		load: bundles.load
+	},
 	layouts: {
 		activate: layouts.activate,
 		register: layouts.register
